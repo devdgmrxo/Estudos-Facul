f93dbfaa9ad93e910c2aa9cd0818de381c6cea7c
devdgmrxo <dagmareleuterio@gmail.com>
2025-08-18 18:30:02 -0300

    Create convertendo-variaveis.c
    convertendo valores de variaveis

---
+++ convertendo-variaveis.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main(){
+
+int a = 10, b =3;
+int soma = a + b;
+int dif = a -b;
+int mult = a * b;
+int divi = a / b;
+
+printf("Soma: %d\n", soma);
+printf("Diferenca:%d\n", dif);
+printf("Multiplicacao:%d\n", mult);
+printf("Divisao:%d\n", divi);
+
+
+
+
+
+
+
+return 0;
+
+
+}
