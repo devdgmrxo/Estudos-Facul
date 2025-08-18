b6bcc76e399970f8ea39882463443c11966490b8
devdgmrxo <dagmareleuterio@gmail.com>
2025-08-18 23:05:18 +0000

    config exatas

--- media-alunos.c
+++ media-alunos.c
@@ -1,27 +1,28 @@
 #include <stdio.h>
 
-float prt, mtm, hist, geo, ing;
-float media;
+//declarando variaveis
 
+int prt, mtm, hist, geo, ing;
+float media;
 
 //função para entrada de dados
 
 void coletarNotas(void){
 
     printf("Insira a nota de Portugues:\n");
-    scanf("%.2f", prt);
+    scanf("%d", &prt);
 
     printf("Insira a nota de Matematica:\n");
-    scanf("%.2f", mtm);
+    scanf("%d", &mtm);
 
     printf("Insira a nota de Historia:\n");
-    scanf("%.2f", hist);
+    scanf("%d", &hist);
 
     printf("Insira a nota de Geografia:\n");
-    scanf("%.2f", geo);
+    scanf("%d", &geo);
 
     printf("Insira a nota de Ingles:\n");
-    scanf("%.2f", ing);
+    scanf("%d", &ing);
 
 }
 
@@ -32,11 +33,25 @@ void mostrarMedia(void){
     printf("---------------\n");
     printf("Notas do aluno:\n");
     printf("---------------\n");
-    printf("Portugues:%.2f\n", prt);
-    printf("Matematica:%.2f\n", mtm);
-    printf("Historia:%.2f\n", hist);
-    printf("Geografia:%.2f\n", geo);
-    printf("Ingles:%.2f\n", ing);
+    printf("Portugues:%d\n", prt);
+    printf("Matematica:%d\n", mtm);
+    printf("Historia:%d\n", hist);
+    printf("Geografia:%d\n", geo);
+    printf("Ingles:%d\n", ing);
     printf("---------------\n");
 
+//calculando médias
+
+media = (float)(prt + mtm + hist + geo + ing) / 5;
+
+    printf("A media desse aluno é: %.2f\n", media);
+
+}
+
+int main(){
+
+    coletarNotas();
+    mostrarMedia();
+
+return 0;    
 }
